53dfa2fff7536d3d84f9bd30dfa85467538db398
shaydaroglu <sercanhaydaroglu@outlook.com>
2026-03-28 02:40:57 +0100

    Initial commit

---
+++ src/main/java/com/sercan/tictactoe_basic/TictactoeBasicApplication.java
@@ -0,0 +1,13 @@
+package com.sercan.tictactoe_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeBasicApplication.class, args);
+	}
+
+}
